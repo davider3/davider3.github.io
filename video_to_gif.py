1e5588e2c1149080188d915674c2c26f2c97e921
David <dreinhardt389@gmail.com>
2024-09-17 11:35:45 +0100

    gif python

--- video_to_gif.py
+++ video_to_gif.py
@@ -2,6 +2,8 @@
 from moviepy.editor import VideoFileClip
 
 # %% CONVERT
-videoClip = VideoFileClip("./assets/images/test_drive.mp4")
-videoClip.write_gif("./assets/images/test_drive.gif")
+videoClip = VideoFileClip("./assets/images/537_arm.mp4")
+videoClip.write_gif("./assets/images/537_arm.gif")
+# %%
+videoClip.speedx(2).to_gif("./assets/images/537_arm2.gif")
 # %%
